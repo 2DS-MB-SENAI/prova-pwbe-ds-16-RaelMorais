8043849fcf927250082c8a01a4e7ebf194296463
Israel <israel@maiil.com>
2025-04-08 10:10:49 -0300

    teste

--- agenda/urls.py
+++ agenda/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('servicos/', views.api_servicos, name="GET e POST serviços"),
-    path('servicos/<int:pk>', views.servico_por_id, name="Serviço por ID"), 
+    path('servicos/<int:pk>/', views.servico_por_id, name="Serviço por ID"), 
 
     path('agendamentos/', views.api_agendamentos, name="Criar agendamento"),
-    path('agendamentos/<int:pk>', views.agendamento_por_id, name="Agendamento por ID"),
+    path('agendamentos/<int:pk>/', views.agendamento_por_id, name="Agendamento por ID"),
 ]
